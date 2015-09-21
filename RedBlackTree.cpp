f05ed8ff246dccc3f977e160aadba25ea3116513
Shubham K Gogna <s-gogna@users.noreply.github.com>
2015-09-21 13:18:37 -0700

    Added ::doRemoveCases()

--- RedBlackTree.cpp
+++ RedBlackTree.cpp
@@ -3,10 +3,10 @@
 
 Operation  |  Average  |  Worst
 -------------------------------------------------------------------------------
-Insertion  |           |
-Deletion   |           |
-Access     |           |
-Search     |           |
+Insertion  |  O(log n) |  O(log n)
+Deletion   |  O(log n) |  O(log n)
+Access     |  --       |  --
+Search     |  O(log n) |  O(log n)
 *******************************************************************************/
 
 template <typename T>
@@ -43,7 +43,7 @@ class RedBlackTree
 
             // "node" is assigned the newly created node
 
-            // recolor(node)
+            // doInsertCases(node)
 
          // Else
             // If the input is "lesser" than the value of node
@@ -58,7 +58,60 @@ class RedBlackTree
 
       bool removeHelper(BinaryTreeNode<T>*& node, const T& key)
       {
-         // TODO
+         // If node is null
+            // Return false (not found)
+
+         // Else
+            // If the key is "lesser" than the value of node
+               // return removeHelper(node -> left, key)
+
+            // Else if the key is "greater" than the value of the node
+               // return removeHelper(node -> right, key)
+
+            // Else
+               // If leaf node (NULL left and NULL right pointers)
+                  // If the node is red
+                     // Deallocate and set node pointer to NULL
+
+                  // If the node is black
+                     // doRemoveCases()
+
+                  // Deallocate and set node pointer to NULL
+
+               // Else if left == NULL && right != NULL
+                  // replace(node, node -> right)
+
+                  // If the original node was black
+                     // If the new node is red
+                        // Recolor the new node to black
+
+                     // Else
+                        // doRemoveCases(new-node)
+
+                  // Deallocate the original node
+
+               // Else if left != NULL && right == NULL
+                  // replace(node, node -> left)
+
+                  // If the original node was black
+                     // If the new node is red
+                        // Recolor the new node to black
+
+                     // Else
+                        // doRemoveCases(new-node)
+
+                  // Deallocate the original node
+
+               // Else
+                  // Find the largest predecessor (LP)
+                  // Follow the left node once and loop to the "deepest" right node
+
+                  // Node copies the value of LP
+                  // Pointers and color of node remain the same
+
+                  // Remove the LP
+
+               // Return true
       }
 
       bool findHelper(BinaryTreeNode<T>*& node, const T& key)
@@ -77,7 +130,7 @@ class RedBlackTree
                // return true (found)
       }
 
-      void verifyColor(BinaryTreeNode<T>*& node)
+      void doInsertCases(BinaryTreeNode<T>*& node)
       {
          // If parent of node is NULL (node is root)
             // Color the node black
@@ -90,7 +143,7 @@ class RedBlackTree
 
             // Recolor grandparent to red
 
-            // verifyColor(grandparent)
+            // doInsertCases(grandparent)
 
          // Else if parent is red and uncle is black
             // If node is a right child and parent is a left child
@@ -114,6 +167,73 @@ class RedBlackTree
 
             // Else
                // rotateRight(parent)
+      }
+
+      void doRemoveCases(BinaryTreeNode<T>*& node)
+      {
+         // NOTE: NULL children are considered black. To prevent
+         // seg. faults, pointer's should be checked before dereferencing.
+
+         // If parent of node is NULL (node is root)
+            // Return since there is nothing to do
+
+         // If sibling is red
+            // Recolor parent to red
+
+            // Recolor sibling to black
+
+            // If node is a left child
+               // rotateLeft(node's parent)
+
+            // Else
+               // rotateRight(node's parent)
+
+         // If parent is black, sibling is black, and
+         // sibling's children are black
+            // Recolor sibling to red
+
+            // doRemoveCases(node's parent)
+
+            // Return
+
+         // If parent is red, sibling is black, and
+         // sibling's children are black
+            // Recolor sibling to red
+
+            // Recolor parent to black
+
+            // Return
+
+         // If sibling is black
+            // If node is a left child, sibling->right is black,
+            // and sibling->left is red
+               // Recolor sibling to red
+
+               // Recolor sibling->left to black
+
+               // rotateRight(sibling)
+
+            // If node is a right child, sibling->left is black,
+            // and sibling->right is red
+               // Recolor sibling to red
+
+               // Recolor sibling->right to black
+
+               // rotateLeft(sibling)
+
+         // Recolor sibling to parent's color
+
+         // Recolor parent to black
+
+         // If node is a left child
+            // Recolor sibling->right to black
+
+            // rotateLeft(node's parent)
+
+         // Else
+            // Recolor sibling->left to black
+
+            // rotateRight(node's parent)
       }
 
       int rotateLeft(BinaryTreeNode<T>*& node)
@@ -150,6 +270,13 @@ class RedBlackTree
          // Node's left-child's right-child becomes left-child of node
 
          // Node becomes right-child of the original left-child
+      }
+
+      void replace(BinaryTreeNode<T>* dest, BinaryTreeNode<T>* src)
+      {
+         // Copy the values and color of src into dest
+
+         // Copy pointers of src into dest
       }
 
       int size;
